3760f31ee1775415abf354177013cd52d4fc967c
LeeChangju <dlfclao@gmail.com>
2025-05-19 14:30:05 +0900

    fix: movement not working due to incorrect use of AddForce
    - Changed movement logic from AddForce to Rigidbody.velocity
    - Ensured directional input directly affects velocity
    - Improved responsiveness and resolved drifting behavior

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,37 +5,54 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerMovement : MonoBehaviour, IPlayerInputReceiver
+public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
-    public float moveSpeed = 5f;
+    private float moveSpeed = 5f;
+    private float iceControlMultiplier = 0.05f;
+    private float airControlMultiplier = 0.5f;
+    private float frictionDamping = 0.9f;
 
     private Vector2 inputDirection;
     private Rigidbody rb;
+
+    private bool isGrounded = false;
+    private bool isOnIce = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
-    {
-        Vector3 move = new Vector3(inputDirection.x, 0f, inputDirection.y);
-        rb.AddForce(move * moveSpeed, ForceMode.Force);
-    }
+    //private void Update()
+    //{
+    //    inputDirection = InputManager.Instance.MoveInput;
+    //}
+
+
 
     public void OnMove(Vector2 direction)
     {
         inputDirection = direction;
     }
-
-    public void OnJump()
+    private void FixedUpdate()
     {
-
+        Vector3 move = new Vector3(inputDirection.x, 0f, inputDirection.y);
+        rb.velocity = new Vector3(move.x * moveSpeed, rb.velocity.y, move.z * moveSpeed);
     }
 
-    public void OnInteract()
+    private void UpdateGroundCheck()
     {
-
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if(Physics.Raycast(ray, out RaycastHit hit, 1.1f))
+        {
+            isGrounded = true;
+            isOnIce = hit.collider.CompareTag("Ice");
+        }
+        else
+        {
+            isGrounded= false;
+            isOnIce= false;
+        }
     }
 }
